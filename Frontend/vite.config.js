099c1724954b75ada5773ebf13afa9546f1945cd
bhargav <bhargavparab05@gmail.com>
2024-08-19 01:13:15 +0530

    updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy : "https://streamnest-3hxb.onrender.com"
+    proxy : "https://streamnest-3hxb.onrender.com/api"
   },
   plugins: [react()],
 })
