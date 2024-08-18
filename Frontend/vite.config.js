8e6e5756c1d61999aeb3c74338cfe23aea7a0d96
bhargav2402 <143709019+bhargav2402@users.noreply.github.com>
2024-08-19 00:28:40 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy : {
-      "/api" : process.env.VITE_PROXY
+      "/api" : "https://streamnest-3hxb.onrender.com"
     }
   },
   plugins: [react()],
